ca5b90e85c4ae8ed900a515ad6d7b9819986522b
BUzunovic1 <148277728+BUzunovic1@users.noreply.github.com>
2025-09-06 03:35:28 +0200

    Add global CSS and block browser zoom shortcuts
    Introduced App.css with base styles to remove default margins, set full width/height, and hide horizontal overflow. Updated App.jsx to import the CSS and added useEffect hooks to prevent zooming via Ctrl+Scroll and Ctrl+'+'/'-' keyboard shortcuts for improved layout control.

--- unsa-racing-webpage/src/App.jsx
+++ unsa-racing-webpage/src/App.jsx
@@ -1,7 +1,30 @@
 import {Header, Footer, Home, News, Subteams, Sponsors} from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import './App.css'
 
 function App() {
+    useEffect(() => {
+    // Blokira Ctrl + Scroll (desktop)
+    const wheelHandler = (e) => {
+      if (e.ctrlKey) e.preventDefault();
+    };
+    // Blokira Ctrl + + / Ctrl + -
+    const keyHandler = (e) => {
+      if ((e.ctrlKey || e.metaKey) && (e.key === "+" || e.key === "-" || e.key === "=")) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("wheel", wheelHandler, { passive: false });
+    document.addEventListener("keydown", keyHandler);
+
+    return () => {
+      document.removeEventListener("wheel", wheelHandler);
+      document.removeEventListener("keydown", keyHandler);
+    };
+  }, []);
+
   return (
     <>
     <Header />
