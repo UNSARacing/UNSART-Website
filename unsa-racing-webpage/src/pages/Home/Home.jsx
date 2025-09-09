007bec6f2f730cc5aee9ade9663c983c2835f670
BUzunovic1 <148277728+BUzunovic1@users.noreply.github.com>
2025-09-09 02:57:44 +0200

    Refactor Home page layout and update header styles
    Improved the Home page structure by reorganizing image elements, updating class names, and enhancing the visual hierarchy with new gradients and layout adjustments in Home.css and Home.jsx. Updated Header.css for better logo scaling, navigation link styling, and responsive header height. These changes aim to modernize the UI and improve maintainability.

--- unsa-racing-webpage/src/pages/Home/Home.jsx
+++ unsa-racing-webpage/src/pages/Home/Home.jsx
@@ -2,25 +2,40 @@ import './Home.css'
 
 const Home = () => {
     return (
+        <>
         <div className="homeContainer">
             <img
                 className="homeBackgroundImage"
-                src="../src/assets/Home/image.png"
+                src="../src/assets/Home/garage.svg"
+                alt="Garage"
             />
-            <img 
-                className="homeDragonLogo"
-                src="../src/assets/Home/teamlogo.png"
-            />
-            <img 
-                className="homeCarImage"
-                src="../src/assets/Home/car.png"
-            />
+
+            {/* KONTEJNER ZA DRAGON I CAR */}
+            <div className="homeLogoGroup">
+                <img 
+                    className="homeDragonLogo"
+                    src="../src/assets/Home/dragon.svg"
+                    alt="Dragon Logo"
+                />
+                <img 
+                    className="homeCarImage"
+                    src="../src/assets/Home/car.svg"
+                    alt="Car"
+                />
+            </div>
+
             <div className="homeContent">
                 <h1 className="homeTitle">UNSA Racing Team</h1>
                 <p className="homeSubtitle">Engineering inovation. Driving Passion.</p>
                 <a href="#learn-more" className="homeButton">Meet the Team</a>
             </div>
+            <div className="teamContainer">
+                {/*
+                    kontejner za tim
+                */}
+            </div>
         </div>
+        </>
     );
 }
 
