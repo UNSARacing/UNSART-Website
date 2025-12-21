05799254ce6a990f3b7dc84f1585abdb58c77d5c
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2025-12-21 01:48:05 +0100

    izbrisani primjeri postova

--- unsa-racing-webpage/src/data/blogPosts.js
+++ unsa-racing-webpage/src/data/blogPosts.js
@@ -2,21 +2,6 @@
 // Each post should have: id, title, date, author, excerpt, image (optional), category (optional)
 
 export const blogPosts = [
-  {
-    id: 'primjer1',
-    title: 'Primjer blog posta 1',
-    date: '2024-09-14',
-    author: 'Kemal Becarevic',
-    excerpt: 'Ovo je primjer nekog blog posta',
-    image: '../src/assets/Home/team-photo.jpg'
-  },
-  {
-    id: 'primjer2',
-    title: 'Primjer blog posta 2',
-    date: '2024-09-15',
-    author: 'Kemal Becarevic',
-    excerpt: 'Ovo je primjer nekog blog posta 2',
-    image: '../src/assets/Home/team-photo2.webp'
-  },
+  
 ];
 
